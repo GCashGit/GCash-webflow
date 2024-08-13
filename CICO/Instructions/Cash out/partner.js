9bc02a87f8687f97838104ae6f4c48cc9b534369
GCashGit <130949627+GCashGit@users.noreply.github.com>
2024-08-13 15:27:32 +0800

    Update partner.js

--- CICO/Instructions/Cash out/partner.js
+++ CICO/Instructions/Cash out/partner.js
@@ -1,1 +1,295 @@
+const CASHOUT = {
+    // OTC Partners
+    
+    'bayadcenter': {
+        name: 'Bayad Center',
+        type: 'OTC',                                                                           
+        instructions: [
+            'Fill out the GCash Service Form.',
+            'Present valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Receive a text message verifying your cash out and confirm with MPIN or OTP.',
+            'Receive cash from the cashier.',
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2019/08/Bayad-Center@3x-1.png',
+        //mapbutton: 'https://www.gcash.com/app/cico/map/?cash-out-partner=bayad-center',
+    },
+    'cebuana': {
+        name: 'Cebuana',
+        type: 'OTC',
+        instructions: [
+            'Inform the cashier that you would like to Cash Out from your GCash wallet.',
+            'Provide your mobile number and desired Cash Out amount.',
+            'Present a valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Wait for an SMS confirming your Cash-Out. Reply with the OTP to confirm.',
+            'Receive the cash from the cashier once you have confirmed.',
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2019/08/Cebuana@3x.png',
+        //mapbutton: '',
+    },
+    'cvm-pawnshop': {
+        name: 'CVM Pawnshop',
+        type: 'OTC',
+        instructions: [
+            'Inform the cashier that you would like to Cash Out from your GCash wallet.',
+            'Provide your mobile number and desired Cash Out amount.',
+            'Present a valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Wait for an SMS confirming your Cash-Out. Reply with the OTP to confirm.',
+            'Receive the cash from the cashier once you have confirmed.'
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2021/06/CVM-Pawnshop@3x.png',
+        //mapbutton: '',
+    },
+    'da5': {
+        name: 'DA5',
+        type: 'OTC',
+        instructions: [
+            'Inform the cashier that you would like to Cash Out from your GCash wallet.',
+            'Provide your mobile number and desired Cash Out amount.',
+            'Present a valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Wait for an SMS confirming your Cash-Out. Reply with the OTP to confirm.',
+            'Receive the cash from the cashier once you have confirmed.'
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2021/05/DA5@3x.png',
+        //mapbutton: '',
+    },
+    'ecpay': {
+        name: 'ECPay',
+        type: 'OTC',
+        instructions: [
+            'Inform the cashier that you would like to Cash Out from your GCash wallet.',
+            'Provide your mobile number and desired Cash Out amount.',
+            'Present a valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Wait for an SMS confirming your Cash-Out. Reply with the OTP to confirm.',
+            'Receive the cash from the cashier once you have confirmed.'
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2019/08/ECPay@3x.png',
+        //mapbutton: '',
+    },
+    'expresspay': {
+        name: 'Express Pay',
+        type: 'OTC',
+        instructions: [
+            'Inform the cashier that you would like to Cash Out from your GCash wallet.',
+            'Provide your mobile number and desired Cash Out amount.',
+            'Present a valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Wait for an SMS confirming your Cash-Out. Reply with the OTP to confirm.',
+            'Receive the cash from the cashier once you have confirmed.'
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2019/08/ExpressPay@3x.png',
+        //mapbutton: 'https://www.gcash.com/app/cico/map/?cash-out-partner=expresspay',
+    },
+    'familymart': {
+        name: 'Family Mart',
+        type: 'OTC',
+        instructions: [
+            'Inform the cashier that you would like to Cash Out from your GCash wallet.',
+            'Provide your mobile number and desired Cash Out amount.',
+            'Present a valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Wait for an SMS confirming your Cash-Out. Reply with the OTP to confirm.',
+            'Receive the cash from the cashier once you have confirmed.'
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2020/12/CashinFamilyMart.png',
+        //mapbutton: '',
+    },
+    'jaro-pawnshop': {
+        name: 'Jaro Pawnshop',
+        type: 'OTC',
+        instructions: [
+            'Inform the cashier that you would like to Cash Out from your GCash wallet.',
+            'Provide your mobile number and desired Cash Out amount.',
+            'Present a valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Wait for an SMS confirming your Cash-Out. Reply with the OTP to confirm.',
+            'Receive the cash from the cashier once you have confirmed.'
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2020/12/JaroPawnshop@2x.png',
+        //mapbutton: '',
+    },
+    'palawan': {
+        name: 'Palawan',
+        type: 'OTC',
+        instructions: [
+            'Fill out the GCash Service Form.',
+            'Present valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Receive a text message verifying your cash out and confirm with MPIN or OTP.',
+            'Receive cash from the cashier.'
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2019/08/Palawan@3x.png',
+        //mapbutton: 'https://www.gcash.com/app/cico/map/?cash-out-partner=palawan',
+    },
+    'panalo-express': {
+        name: 'Panalo Express',
+        type: 'OTC',
+        instructions: [
+            'Inform the cashier that you would like to Cash Out from your GCash wallet.',
+            'Provide your mobile number and desired Cash Out amount.',
+            'Present a valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Wait for an SMS confirming your Cash-Out. Reply with the OTP to confirm.',
+            'Receive the cash from the cashier once you have confirmed.'
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2021/05/PanaloExpress@3x.png',
+        //mapbutton: '',
+    },
+    'perahub': {
+        name: 'Perahub',
+        type: 'OTC',
+        instructions: [
+            'Inform the agent that you would like to Cash Out from your GCash wallet.',
+            'Give your GCash-registered mobile number and your cash out request.',
+            'Present valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Wait for an SMS confirming your Cash Out. Reply with your OTP to confirm.',
+            'Receive the cash from the cashier once you have confirmation'
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2020/12/Perahub@2x.png',
+        //mapbutton: '',
+    },
+    'posible': {
+        name: 'Posible',
+        type: 'OTC',
+        instructions: [
+            'Inform the agent that you would like to Cash Out from your GCash wallet.',
+            'Give your GCash-registered mobile number and your cash out request.',
+            'Present valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Wait for an SMS confirming your Cash Out. Reply with your OTP to confirm.',
+            'Receive the cash from the cashier once you have confirmation'
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2019/12/Posible@3x.png',
+        //mapbutton: '',
+    },
+    'puregold': {
+        name: 'Puregold',
+        type: 'OTC',
+        instructions: [
+            'Fill out the GCash Service Form.',
+            'Present valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Receive a text message verifying your cash out and confirm with MPIN or OTP.',
+            'Receive cash from the cashier.'
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2019/08/Puregold@3x-150x150.png',
+        //mapbutton: 'https://www.gcash.com/app/cico/map/?cash-out-partner=puregold',
+    },
+    'rd-pawnshop': {
+        name: 'RD Pawnshop',
+        type: 'OTC',
+        instructions: [
+            'Inform the cashier that you would like to Cash Out from your GCash wallet.',
+            'Provide your mobile number and desired Cash Out amount.',
+            'Present a valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Wait for an SMS confirming your Cash-Out. Reply with the OTP to confirm.',
+            'Receive the cash from the cashier once you have confirmed.'
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2020/07/RD-Pawnshop.png',
+        //mapbutton: '',
+    },
+    'robinsons': {
+        name: 'Robinsons',
+        type: 'OTC',
+        instructions: [
+            'Fill out the GCash Service Form.',
+            'Present valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Receive a text message verifying your cash out and confirm with MPIN or OTP.',
+            'Receive cash from the cashier.'
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2019/08/Robinsons@3x-150x150.png',
+        //mapbutton: 'https://www.gcash.com/app/cico/map/?cash-out-partner=robinsons',
+    },
+    'sm': {
+        name: 'SM',
+        type: 'OTC',
+        instructions: [
+            'Inform the cashier that you would like to Cash Out from your GCash wallet.',
+            'Provide your mobile number and desired Cash Out amount.',
+            'Present a valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Wait for an SMS confirming your Cash-Out. Reply with the OTP to confirm.',
+            'Receive the cash from the cashier once you have confirmed.'
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2019/08/SM@3x.png',
+        //mapbutton: 'https://www.gcash.com/app/cico/map/?cash-out-partner=sm',
+    },
+    'tambunting': {
+        name: 'Tambunting',
+        type: 'OTC',
+        instructions: [
+            'Fill out the GCash Service Form.',
+            'Present valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Receive a text message verifying your cash out and confirm with MPIN or OTP.',
+            'Receive cash from the cashier.'
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2019/08/Tambunting@3x.png',
+        //mapbutton: 'https://www.gcash.com/app/cico/map/?cash-out-partner=tambunting',
+    },
+    'truemoney': {
+        name: 'True Money',
+        type: 'OTC',
+        instructions: [
+            'Inform the agent that you would like to Cash Out from your GCash wallet.',
+            'Give your GCash-registered mobile number and your cash out request.',
+            'Present valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Wait for an SMS confirming your Cash Out. Reply with your OTP to confirm.',
+            'Receive the cash from the cashier once you have confirmation'
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2020/12/TrueMoney@2x.png',
+        //mapbutton: '',
+    },
+    'villarica': {
+        name: 'Villarica',
+        type: 'OTC',
+        instructions: [
+            'Fill out the GCash Service Form.',
+            'Present valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Receive a text message verifying your cash out and confirm with MPIN or OTP.',
+            'Receive cash from the cashier.'
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2019/08/Villarica@3x.png',
+        //mapbutton: 'https://www.gcash.com/app/cico/map/?cash-out-partner=villarica',
+    },
+    'vip-payments-center': {
+        name: 'VIP Payments Center',
+        type: 'OTC',
+        instructions: [
+            'Inform the cashier that you would like to Cash Out from your GCash wallet.',
+            'Provide your mobile number and desired Cash Out amount.',
+            'Present a valid ID. <br><a href="https://www.beta.gcash.com/cico/valid-ids">See list of valid IDs ></a>',
+            'Wait for an SMS confirming your Cash-Out. Reply with the OTP to confirm.',
+            'Receive the cash from the cashier once you have confirmed.'
+        ],
+        logo: 'https://www.gcash.com/wp-content/uploads/2020/12/VipPaymentsCenter@2x.png',
+        //mapbutton: '',
+    },
 
+    // Barcode Partners
+
+    'test-partner': {
+        name: 'Test Partner',
+        type: 'barcode',
+        logo: 'https://www.gcash.com/wp-content/uploads/2020/12/VipPaymentsCenter@2x.png',
+        //mapbutton: '',
+    },
+    'm-lhuillier': {
+        name: 'M Lhuillier',
+        type: 'OTC',
+        instructions: [
+            'Inform the cashier that you would like to Cash Out from your GCash wallet.',
+            'Provide your mobile number and desired Cash Out amount.',
+            'Present a valid ID.',
+            'Wait for an SMS confirming your Cash-Out. Reply with the OTP to confirm.',
+            'Receive the cash from the cashier once you have confirmed.',
+        ],
+        logo: 'https://uat.m.gcash.com/gcash-common-web/static/cashin_icons/revamp/MLhuillier%403x.png',
+        //mapbutton: '',
+    },
+    'gcash-po': {
+        name: 'GCash Pera Outlet',
+        listType: 'OL_DISC',
+        type: 'OTC',
+        instructions: [
+            'Inform the cashier that you would like to Cash Out from your GCash wallet.',
+            'Provide your mobile number and desired Cash Out amount.',
+            'Present a valid ID.',
+            'Wait for an SMS confirming your Cash Out. Reply with the OTP to confirm.',
+            'Receive the cash from the cashier once you have confirmed.',
+        ],
+        logo: 'https://uat.m.gcash.com/gcash-common-web/static/cashin_icons/revamp/GCashPO%403x.png',
+        //mapbutton: '',
+    },
+
+};
