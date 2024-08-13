6b3182b57e97b9480eba7dbbd8eb937cb4542240
GCashGit <130949627+GCashGit@users.noreply.github.com>
2024-08-13 15:21:29 +0800

    Update partner.js

--- CICO/Multiple/Cash in/partner.js
+++ CICO/Multiple/Cash in/partner.js
@@ -1,1 +1,149 @@
 
+const multiple = {
+        'test-partner': {
+        name: 'Test Partner',
+        instructions: {
+            'Generate-Personal-QR': {
+                imageUrl: 'https://www.gcash.com/wp-content/uploads/2022/06/Personal-QR@3x-1.png',
+                content: [
+                    '<ol>',
+                    '<li>Inform cashier that you would like to Cash In to your GCash wallet.</li>',
+                    '<li>Tap on ÃƒÂƒÃ‚ÂƒÃƒÂ†Ã‚Â’ÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â¢ÃƒÂƒÃ‚ÂƒÃƒÂ‚Ã‚Â¢ÃƒÂƒÃ‚Â¢ÃƒÂ¢Ã‚Â‚Ã‚Â¬ÃƒÂ…Ã‚Â¡ÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â¬ÃƒÂƒÃ‚ÂƒÃƒÂ¢Ã‚Â€Ã‚Â¦ÃƒÂƒÃ‚Â¢ÃƒÂ¢Ã‚Â‚Ã‚Â¬ÃƒÂ…Ã‚Â“Generate Personal QRÃƒÂƒÃ‚ÂƒÃƒÂ†Ã‚Â’ÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â¢ÃƒÂƒÃ‚ÂƒÃƒÂ‚Ã‚Â¢ÃƒÂƒÃ‚Â¢ÃƒÂ¢Ã‚Â‚Ã‚Â¬ÃƒÂ…Ã‚Â¡ÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â¬ÃƒÂƒÃ‚ÂƒÃƒÂ¢Ã‚Â€Ã‚ÂšÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â button below. Add the amount then tap ÃƒÂƒÃ‚ÂƒÃƒÂ†Ã‚Â’ÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â¢ÃƒÂƒÃ‚ÂƒÃƒÂ‚Ã‚Â¢ÃƒÂƒÃ‚Â¢ÃƒÂ¢Ã‚Â‚Ã‚Â¬ÃƒÂ…Ã‚Â¡ÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â¬ÃƒÂƒÃ‚ÂƒÃƒÂ¢Ã‚Â€Ã‚Â¦ÃƒÂƒÃ‚Â¢ÃƒÂ¢Ã‚Â‚Ã‚Â¬ÃƒÂ…Ã‚Â“SaveÃƒÂƒÃ‚ÂƒÃƒÂ†Ã‚Â’ÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â¢ÃƒÂƒÃ‚ÂƒÃƒÂ‚Ã‚Â¢ÃƒÂƒÃ‚Â¢ÃƒÂ¢Ã‚Â‚Ã‚Â¬ÃƒÂ…Ã‚Â¡ÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â¬ÃƒÂƒÃ‚ÂƒÃƒÂ¢Ã‚Â€Ã‚ÂšÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â. Show the cashier the generated QR in the app.</li>',
+                    '<li>Cashier to confirm, collect payment and print receipt. Wait for a text confirmation upon successful Cash In before leaving the store.</li>',
+                    '</ol>',
+                    '<a class="generate-btn" href="#"> GENERATE PERSONAL QR </a>'
+                ]
+            },
+            'Generate-Barcode': {
+                imageUrl: 'https://www.gcash.com/wp-content/uploads/2022/06/Barcode@3x.png',
+                content: [
+                    '<ol>',
+                    '<li>Inform cashier that you would like to Cash In to your GCash wallet.</li>',
+                    '<li>Tap on ÃƒÂƒÃ‚ÂƒÃƒÂ†Ã‚Â’ÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â¢ÃƒÂƒÃ‚ÂƒÃƒÂ‚Ã‚Â¢ÃƒÂƒÃ‚Â¢ÃƒÂ¢Ã‚Â‚Ã‚Â¬ÃƒÂ…Ã‚Â¡ÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â¬ÃƒÂƒÃ‚ÂƒÃƒÂ¢Ã‚Â€Ã‚Â¦ÃƒÂƒÃ‚Â¢ÃƒÂ¢Ã‚Â‚Ã‚Â¬ÃƒÂ…Ã‚Â“Generate BarcodeÃƒÂƒÃ‚ÂƒÃƒÂ†Ã‚Â’ÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â¢ÃƒÂƒÃ‚ÂƒÃƒÂ‚Ã‚Â¢ÃƒÂƒÃ‚Â¢ÃƒÂ¢Ã‚Â‚Ã‚Â¬ÃƒÂ…Ã‚Â¡ÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â¬ÃƒÂƒÃ‚ÂƒÃƒÂ¢Ã‚Â€Ã‚ÂšÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â button below. Enter the amount then tap ÃƒÂƒÃ‚ÂƒÃƒÂ†Ã‚Â’ÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â¢ÃƒÂƒÃ‚ÂƒÃƒÂ‚Ã‚Â¢ÃƒÂƒÃ‚Â¢ÃƒÂ¢Ã‚Â‚Ã‚Â¬ÃƒÂ…Ã‚Â¡ÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â¬ÃƒÂƒÃ‚ÂƒÃƒÂ¢Ã‚Â€Ã‚Â¦ÃƒÂƒÃ‚Â¢ÃƒÂ¢Ã‚Â‚Ã‚Â¬ÃƒÂ…Ã‚Â“Generate BarcodeÃƒÂƒÃ‚ÂƒÃƒÂ†Ã‚Â’ÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â¢ÃƒÂƒÃ‚ÂƒÃƒÂ‚Ã‚Â¢ÃƒÂƒÃ‚Â¢ÃƒÂ¢Ã‚Â‚Ã‚Â¬ÃƒÂ…Ã‚Â¡ÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â¬ÃƒÂƒÃ‚ÂƒÃƒÂ¢Ã‚Â€Ã‚ÂšÃƒÂƒÃ‚Â‚ÃƒÂ‚Ã‚Â. Show the cashier the generated barcode in the app.</li>',
+                    '<li>Cashier to confirm, collect payment and print receipt. Wait for a text confirmation upon successful Cash In before leaving the store.</li>',
+                    '</ol>',
+                    '<a class="generate-btn" href="#"> GENERATE BARCODE </a>'
+                ]
+            },
+            'via-POS-machine': {
+                imageUrl:'https://www.gcash.com/wp-content/uploads/2021/06/partner_logo.png',
+                content: [  
+                    '<ol>',
+                    '<li>Select GCash Cash In > Cash In on the machine.</li>',
+                    '<li>Enter your 11-digit GCash mobile number and cash in amount.</li>',
+                    '<li>Insert cash payment.</li>',
+                    '<li>Receive receipt from the machine and a text confirmation.</li>',
+                    '</ol>'
+                ]
+            },
+            'via-Mobile-Number': {
+                imageUrl: 'https://www.gcash.com/wp-content/uploads/2021/03/PhoneInHand60x60.jpg',
+                content: [
+                    '<ol>',
+                    '<li>Inform the cashier that you would like to Cash In to your GCash wallet.</li>',
+                    '<li>Provide your mobile number and desired Cash In amount.</li>',
+                    '<li>Cashier to confirm, collect payment and print receipt. Wait for a text confirmation upon successful Cash In before leaving the store.</li>',
+                    '</ol>'
+                ]
+            },
+        },
+        logo: 'https://uploads-ssl.webflow.com/64129d0a758f1256bf3e95cc/64129d0a758f12d75f3e960b_header-gcash-logo.svg'
+    },
+    'robinsons-supermarket': {
+        name: 'Robinsons Supermarket',
+        instructions: {
+            'Generate-Barcode': {
+                imageUrl: 'https://www.gcash.com/wp-content/uploads/2022/06/Barcode@3x.png',
+                content: [
+                    '<ol>',
+                    '<li>Inform cashier that you would like to Cash In to your GCash wallet.</li>',
+                    '<li>Tap on the Generate Barcode button below. Enter the amount then tap "Generate Barcode". Show the cashier the generated barcode in the app.</li>',
+                    '<li>Cashier to confirm, collect payment and print receipt. Wait for a text confirmation upon successful Cash In before leaving the store.</li>',
+                    '</ol>',
+                    '<a class="generate-btn" href="#"> GENERATE BARCODE </a>'
+                ]
+            },
+            'via-POS-machine': {
+                imageUrl: 'https://www.gcash.com/wp-content/uploads/2021/06/partner_logo.png',
+                content: [
+                    '<ol>',
+                    '<li>Select GCash Cash In > Cash In on the machine.</li>',
+                    '<li>Enter your 11-digit GCash mobile number and cash in amount.</li>',
+                    '<li>Insert cash payment.</li>',
+                    '<li>Receive receipt from the machine and a text confirmation.</li>',
+                    '</ol>'
+                ]
+            },
+        },
+        logo: 'https://www.gcash.com/wp-content/uploads/2021/07/RobinsonsSupermarket@3x.png'
+    },
+    'bsp-old': {
+        name: 'BSP Coin Deposit Machine',
+        instructions: {
+            'Cash In via Code/Barcode': {
+                content: [
+                    '<div class="barcode-container"><img style="width: 95%;" src="https://uploads-ssl.webflow.com/6385b55675a0bd614777a5c1/66bb0401955807da9e4f4b38_0af0cece61b4224ba56912d1600b6741.png"/></div>',
+                    '<ol>',
+                    '<li>Go to any partner retailer/mall with a BSP Coin Deposit Machine.</li>',
+                    '<li>On the BSP Coin Deposit Machine, select "New Transaction" on the machine.</li>',
+                    '<li>Place your coins in the coin slot marked with &rarr; and press "A".</li>',
+                    '<li>Press the "B" button to proceed to conversion.</li>',
+                    '<li>Select GCash as your target destination and tap "Cash-In Code" as preferred method for GCash cash-in</li>',
+                    '<li>Prepare your cash in code by logging in to GCash, tapping "Cash in", and selecting "BSP Coin Deposit Machine". Tap the "Generate Barcode" button. Enter the amount then tap "Generate Code".</li>',
+                    '<li>On the BSP Coin Deposit Machine, enter the code or scan the barcode generated by your GCash app and tap "Continue".</li>',
+                    '<li>Wait for your transaction receipt and SMS confirmation before leaving.</li>',
+                    '</ol>',
+                ]
+            },
+            'Cash In via Mobile Number': {
+                content: [
+                    '<ol>',
+                    '<li>Go to any partner retailer/mall with a BSP Coin Deposit Machine.</li>',
+                    '<li>On the BSP Coin Deposit Machine, select "New Transaction" on the machine.</li>',
+                    '<li>Place your coins in the coin slot marked with &rarr; and press "A".</li>',
+                    '<li>Press the "B" button to proceed to conversion.</li>',
+                    '<li>Select GCash as your target destination and tap "Mobile Number" as preferred method for GCash cash-in.</li>',
+                    '<li>On the machine, enter your 11-digit GCash mobile number and tap "Continue".</li>',
+                    '<li>Wait for your transaction receipt and SMS confirmation before leaving.</li>',
+                    '</ol>'
+                ]
+            },
+        },
+        logo: 'https://uat.m.gcash.com/gcash-common-web/static/cashin_icons/revamp/BSP%403x.png'
+    },
+    'bsp': {
+        name: 'BSP Coin Deposit Machine',
+        //subHeader: 'Coin Deposit Machine',
+        instructions: {
+            'Cash In via Code/Barcode': {
+                content: [
+                    '<div class="barcode-container"><img style="width: 95%;" src="https://uploads-ssl.webflow.com/6385b55675a0bd614777a5c1/66bb0401955807da9e4f4b38_0af0cece61b4224ba56912d1600b6741.png"/></div>',
+                    '<div class="barcode-ol-container">',
+                    '<div class="step-element ol-disc"><span class="step-number">1</span> <p>Go to any partner retailer/mall with a BSP Coin Deposit Machine.</p></div>',
+                    '<div class="step-element ol-disc"><span class="step-number">2</span> <p>On the BSP Coin Deposit Machine, select "New Transaction" on the machine.</p></div>',
+                    '<div class="step-element ol-disc"><span class="step-number">3</span> <p>Place your coins in the coin slot marked with &rarr; and press "A".</p></div>',
+                    '<div class="step-element ol-disc"><span class="step-number">4</span> <p>Press the "B" button to proceed to conversion.</p></div>',
+                    '<div class="step-element ol-disc"><span class="step-number">5</span> <p>Select GCash as your target destination and tap "Cash-In Code" as preferred method for GCash cash-in</p></div>',
+                    '<div class="step-element ol-disc"><span class="step-number">6</span> <p>Prepare your cash in code by logging in to GCash, tapping "Cash in", and selecting "BSP Coin Deposit Machine". Tap the "Generate Barcode" button. Enter the amount then tap "Generate Barcode".</p></div>',
+                    '<div class="step-element ol-disc"><span class="step-number">7</span> <p>On the BSP Coin Deposit Machine, enter the code or scan the barcode generated by your GCash app and tap "Continue".</p></div>',
+                    '<div class="step-element ol-disc"><span class="step-number">8</span> <p>Wait for your transaction receipt and SMS confirmation before leaving.</p></div>',
+                    '</div>',
+                ]
+            },
+            'Cash In via Mobile Number': {
+                content: [
+                    '<div class="barcode-ol-container">',
+                    '<div class="step-element ol-disc"><span class="step-number">1</span> <p>Go to any partner retailer/mall with a BSP Coin Deposit Machine.</p></div>',
+                    '<div class="step-element ol-disc"><span class="step-number">2</span> <p>On the BSP Coin Deposit Machine, select "New Transaction" on the machine.</p></div>',
+                    '<div class="step-element ol-disc"><span class="step-number">3</span> <p>Place your coins in the coin slot marked with &rarr; and press "A".</p></div>',
+                    '<div class="step-element ol-disc"><span class="step-number">4</span> <p>Press the "B" button to proceed to conversion.</p></div>',
+                    '<div class="step-element ol-disc"><span class="step-number">5</span> <p>Select GCash as your target destination and tap "Mobile Number" as preferred method for GCash cash-in.</p></div>',
+                    '<div class="step-element ol-disc"><span class="step-number">6</span> <p>On the BSP Coin Deposit Machine, enter your 11-digit GCash mobile number and tap "Continue".</p></div>',
+                    '<div class="step-element ol-disc"><span class="step-number">7</span> <p>Wait for your transaction receipt and SMS confirmation before leaving.</p></div>',
+                    '</div>'
+                ]
+            },
+        },
+        logo: 'https://m.gcash.com/gcash-common-web/static/cashin_icons/revamp/BSP%403x.png'
+    },
+};
