be212054288a1483b3a6659064c0cfde9fc03b70
GCashGit <130949627+GCashGit@users.noreply.github.com>
2024-09-12 11:37:12 +0800

    Update renderer.js

--- CICO/Instructions/Cash in/renderer.js
+++ CICO/Instructions/Cash in/renderer.js
@@ -191,7 +191,7 @@ $(document).ready(function() {
          var imageUrl = 'https://uploads-ssl.webflow.com/64129d0a758f1256bf3e95cc/64a2695dd5e9bb672ace7e37_barcode_fullwidth.jpeg'; //'barcode_fullwidth.jpeg';//'https://uploads-ssl.webflow.com/64129d0a758f1256bf3e95cc/64758b6c555ad35ace755328_Cash%20In%20no%20shadow.png';//'https://www.gcash.com/wp-content/uploads/2019/10/barcode_fullwidth.jpg';
       }
         else {
-        var imageUrl = 'https://cdn.prod.website-files.com/6385b55675a0bd614777a5c1/66e260f2d51ac10653b8b59d_Over%20the%20Counter%20Banner.avif';
+        var imageUrl = 'https://cdn.prod.website-files.com/6385b55675a0bd614777a5c1/66e261b9d3d0c50efe2694d2_barcode.avif';
         }
       var linkUrl = 'gcash://com.mynt.gcash/app/006300070012';//'https://gcash.cashin.viacode/gcash.splashscreen/?redirect=gcash://com.mynt.gcash/app/006300080600'; //'https://gcashapp.page.link/cashin_subapp_genericBarcode';
   
