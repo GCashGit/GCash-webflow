37d6377acf8e70beaeaaa55976bbb61a63dce572
GCashGit <130949627+GCashGit@users.noreply.github.com>
2024-10-02 08:49:03 +0800

    Update partner.js

--- CICO/Multiple/Cash in/partner.js
+++ CICO/Multiple/Cash in/partner.js
@@ -1,4 +1,3 @@
-
 const multiple = {
         'test-partner': {
         name: 'Test Partner',
@@ -75,7 +74,8 @@ const multiple = {
                 ]
             },
         },
-        logo: 'https://www.gcash.com/wp-content/uploads/2021/07/RobinsonsSupermarket@3x.png'
+        logo: 'https://www.gcash.com/wp-content/uploads/2021/07/RobinsonsSupermarket@3x.png',
+        type: 'OTC',
     },
     'bsp-old': {
         name: 'BSP Coin Deposit Machine',
@@ -109,7 +109,8 @@ const multiple = {
                 ]
             },
         },
-        logo: 'https://uat.m.gcash.com/gcash-common-web/static/cashin_icons/revamp/BSP%403x.png'
+        logo: 'https://uat.m.gcash.com/gcash-common-web/static/cashin_icons/revamp/BSP%403x.png',
+        type: 'OTC',
     },
     'bsp': {
         name: 'BSP Coin Deposit Machine',
@@ -144,7 +145,8 @@ const multiple = {
                 ]
             },
         },
-        logo: 'https://m.gcash.com/gcash-common-web/static/cashin_icons/revamp/BSP%403x.png'
+        logo: 'https://m.gcash.com/gcash-common-web/static/cashin_icons/revamp/BSP%403x.png',
+        type: 'OTC',
     },
         'uniteller': {
         name: 'uLink/Uniteller',
@@ -152,30 +154,30 @@ const multiple = {
         instructions: {
             'Cash In via uLink SuperApp': {
                 content: [
-                    '<div class="barcode-ol-container">',
-                    '<div class="step-element ol-disc"><span class="step-number">1</span> <p>Senders need to log into their account through the uLink Money Transfer SuperApp or via  <a href="https://ulink.com.">https://ulink.com.</a></p></div>',
-                    '<div class="step-element ol-disc"><span class="step-number">2</span> <p>Tap the send icon.</p></div>',
-                    '<div class="step-element ol-disc"><span class="step-number">3</span> <p>Select or add a new beneficiary.</p></div>',
-                    '<div class="step-element ol-disc"><span class="step-number">4</span> <p>Choose how they’ll receive the money. Select wallet, and then GCash.</p></div>',
-                    '<div class="step-element ol-disc"><span class="step-number">5</span> <p>Enter the beneficiary’s phone number. (Please enter the phone number associated with their GCash wallet. The number must start with 9).</p></div>',
-                    '<div class="step-element ol-disc"><span class="step-number">6</span> <p>Enter the amount and select your payment method.</p></div>',
-                    '<div class="step-element ol-disc"><span class="step-number">7</span> <p>Review and confirm your transfer.</p></div>',
-                    '</div>'
+                    '<ol>',
+                        '<li>Senders need to log into their account through the uLink Money Transfer SuperApp or via https://ulink.com.</li>',
+                        '<li>Tap the send icon</li>',
+                        '<li>Select or add a new beneficiary.</li>',
+                        '<li>Choose how they’ll receive the money. Select wallet, and then GCash.</li>',
+                        '<li>Enter the beneficiary’s phone number. (Please enter the phone number associated with their GCash wallet. The number must start with 9).</li>',
+                        '<li>Enter the amount and select your payment method.</li>',
+                        '<li>Review and confirm your transfer.</li>',
+                    '</ol>'
                 ]
             },
             'Cash In via UniTeller Agent': {
                 content: [
-                    '<div class="barcode-ol-container">',
-                    '<div class="step-element ol-disc"><span class="step-number">1</span> <p>Go to a UniTeller Agent location.</p></div>',
-                    `<div class="step-element ol-disc"><span class="step-number">2</span> <div><p>Fill out the send money form.</p>
-                        <ul style="list-style-type: disc;">
-                            <li>The beneficiary name must match their GCash-registered name.</li>
-                            <li>The account number is the GCash-registered mobile number (in the appropriate 09XXXXXXXXX format).</li>
-                        </ul></div>
-                    </div>`,
-                    '<div class="step-element ol-disc"><span class="step-number">3</span> <p>Pay for the remittance (the amount to send and the transfer fee).</p></div>',
-                    '<div class="step-element ol-disc"><span class="step-number">4</span> <p>The beneficiary will receive an SMS notification from GCash that the remittance was successfully sent to their GCash wallet.</p></div>',
-                    '</div>'
+                    '<ol>',
+                        '<li>Go to a UniTeller Agent location.</li>',
+                        `<li>Fill out the send money form.
+                            <ul style="list-style-type: disc; padding-left: 2rem;">
+                                <li>The beneficiary name must match their GCash-registered name.</li>
+                                <li>The account number is the GCash-registered mobile number (in the appropriate 09XXXXXXXXX format).</li>
+                            </ul>
+                        </li>`,
+                        '<li>Pay for the remittance (the amount to send and the transfer fee).</li>',
+                        '<li>The beneficiary will receive an SMS notification from GCash that the remittance was successfully sent to their GCash wallet.</li>',
+                    '</ol>',
                 ]
             }
         }
